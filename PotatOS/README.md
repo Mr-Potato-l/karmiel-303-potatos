38bef6bb71629b576c608860b2254c056206c208
royalfly <sargit08@gmail.com>
2025-12-02 16:06:47 +0200

    delleted dublicate folder

--- PotatOS/README.md
+++
@@ -1,2 +0,0 @@
-# Main folder for anything related to the OS!
-when you want to compile the program write "./comp" in the terminal
--- PotatOS/src/keyboard.h
+++
@@ -1,9 +0,0 @@
-#ifndef KEYBOARD_H
-#define KEYBOARD_H
-
-#include <stdint.h>
-
-void keyboard_handler();
-void keyboard_poll_test();
-
-#endif
