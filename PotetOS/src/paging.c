f701478cef1cb5aa305d1911c90db8642b5e857b
royalfly <sargit08@gmail.com>
2026-01-04 18:16:04 +0200

    added coments to code

--- PotetOS/src/paging.c
+++ PotetOS/src/paging.c
@@ -1,16 +1,21 @@
 #include "paging.h"
 #include "pmm.h"
 
+// define a simple paging structure with 4KB identity mapping
 uint32_t page_directory[PAGE_ENTRIES] __attribute__((aligned(4096)));
 
+// Load page directory into CR3 (paging manager register)
 static inline void load_page_directory(uint32_t phys_addr) {
     __asm__ volatile("mov %0, %%cr3" :: "r"(phys_addr));
 }
 
 static inline void enable_paging() {
     uint32_t cr0;
+    // clearing the CR0
     __asm__ volatile("mov %%cr0, %0" : "=r"(cr0));
+    // set the paging bit (can't do it directly in inline asm, so we use bitwise or)
     cr0 |= 0x80000000;
+    // write back to CR0
     __asm__ volatile("mov %0, %%cr0" :: "r"(cr0));
 }
 
